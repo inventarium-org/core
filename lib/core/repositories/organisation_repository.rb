6cc27a123b1136b90b077d1591b58069ef8460a9
Anton Davydov <antondavydov.o@gmail.com>
2020-05-01 22:19:33 +0300

    Create organisation repository methods for getting organisation

--- lib/core/repositories/organisation_repository.rb
+++ lib/core/repositories/organisation_repository.rb
@@ -1,7 +1,21 @@
 class OrganisationRepository < Hanami::Repository
   associations do
-
     has_many :account_organisations
     has_many :accounts, through: :account_organisations
+  end
+
+  def all_for_account(account_id)
+    root
+      .join(account_organisations)
+      .where(account_organisations[:account_id] => account_id)
+      .map_to(Organisation).to_a
+  end
+
+  def find_for_account(id, account_id)
+    root
+      .join(account_organisations)
+      .where(account_organisations[:account_id] => account_id)
+      .by_pk(id)
+      .map_to(Organisation).one
   end
 end
--- spec/core/repositories/organisation_repository_spec.rb
+++ spec/core/repositories/organisation_repository_spec.rb
@@ -1,3 +1,53 @@
 RSpec.describe OrganisationRepository, type: :repository do
-  # place your tests here
+  let(:repo) { described_class.new }
+
+  describe '#all_for_account' do
+    let(:account) { Fabricate(:account) }
+    let(:organisation) { Fabricate(:organisation) }
+    let(:empty_account) { Fabricate(:account) }
+
+    before do
+      Fabricate(:account_organisation, account_id: account.id, organisation_id: organisation.id)
+    end
+
+    context 'when user have organisations' do
+      subject { repo.all_for_account(account.id) }
+
+      it { expect(subject).to eq([organisation]) }
+    end
+
+    context 'when user does not have any organisations' do
+      subject { repo.all_for_account(empty_account.id) }
+
+      it { expect(subject).to eq([]) }
+    end
+  end
+
+  describe '#find_for_account' do
+    let(:account) { Fabricate(:account) }
+    let(:organisation) { Fabricate(:organisation) }
+    let(:empty_account) { Fabricate(:account) }
+
+    before do
+      Fabricate(:account_organisation, account_id: account.id, organisation_id: organisation.id)
+    end
+
+    context 'when user have organisations' do
+      subject { repo.find_for_account(organisation.id, account.id) }
+
+      it { expect(subject).to eq(organisation) }
+    end
+
+    context 'when user does not have organisations' do
+      subject { repo.find_for_account(0, account.id) }
+
+      it { expect(subject).to eq(nil) }
+    end
+
+    context 'when organisation is not exist' do
+      subject { repo.find_for_account(organisation.id, empty_account.id) }
+
+      it { expect(subject).to eq(nil) }
+    end
+  end
 end
