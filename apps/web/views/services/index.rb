4a0ff11ca81443c6b1364120efce6b1b247fabbf
Anton Davydov <antondavydov.o@gmail.com>
2020-05-05 02:13:38 +0300

    Implement readiness check logic on services index page

--- apps/web/views/services/index.rb
+++ apps/web/views/services/index.rb
@@ -6,6 +6,8 @@ module Web
       class Index
         include Web::View
 
+        READINESS_CHECKS_COUNT = Readiness::CHECK_NAMES.count
+
         def breadcrumb
           'Services'
         end
@@ -16,6 +18,30 @@ module Web
 
         def link_to_service(service)
           link_to service, "/#{organisation.slug}/services/#{service}"
+        end
+
+        def readinessbar(service)
+          completed_checks_count = service.readiness.completed_checks_count
+          percent = (completed_checks_count / READINESS_CHECKS_COUNT.to_f) * 100.0
+
+          html.div(class: 'row') do
+            div(class: 'col-8') do
+              div(class: 'progress') do
+                div(
+                  class: 'progress-bar bg-success',
+                  'aria-valuemax': "100",
+                  'aria-valuemin': "0",
+                  'aria-valuenow': "100",
+                  role: "progressbar",
+                  style: "width: #{percent}%"
+                )
+              end
+            end
+
+            div(class: 'col-4') do
+              span { "#{completed_checks_count} / #{READINESS_CHECKS_COUNT}" }
+            end
+          end
         end
       end
     end
