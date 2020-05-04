8e9dfa0377686c7167ad11dcfc7ddc9282befa4b
Anton Davydov <antondavydov.o@gmail.com>
2020-05-04 21:08:44 +0300

    Create simple implementation of show page

--- apps/web/views/services/show.rb
+++ apps/web/views/services/show.rb
@@ -10,7 +10,27 @@ module Web
           html do
             a('Services', href: "/#{organisation.slug}/services")
             text('>')
-            span('Service name')
+            span(service.name)
+          end
+        end
+
+        def service_information(key, value)
+          html.div(class: 'row') do
+            div(class: 'col') { key }
+            div(class: 'col') { value || 'Empty' }
+          end
+        end
+
+        def service_information_link(key, value)
+          html.div(class: 'row') do
+            div(class: 'col') { key }
+            div(class: 'col') do
+              if value
+                link_to URI(value).host, value
+              else
+                'Empty'
+              end
+            end
           end
         end
       end
