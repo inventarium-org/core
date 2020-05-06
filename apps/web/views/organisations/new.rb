3c1afc10db191ed7d03614a9add14db449ab0256
Anton Davydov <antondavydov.o@gmail.com>
2020-05-06 23:33:25 +0300

    Create simple form for creating organisations

--- apps/web/views/organisations/new.rb
+++ apps/web/views/organisations/new.rb
@@ -11,6 +11,18 @@ module Web
         def new_organisation_active_class
           'active'
         end
+
+        def form
+          form_for :organisation, routes.organisations_path, method: :post, class: 'form-inline needs-validation', novalidate: true do
+            text_field :account_id, type: 'hidden', value: current_account.id
+
+            div(class: 'form-group mr-2') do
+              text_field :name, placeholder: 'Organisation name', class: 'form-control', required: true
+            end
+
+            submit 'Create organisation', class: 'btn btn-success'
+          end
+        end
       end
     end
   end
