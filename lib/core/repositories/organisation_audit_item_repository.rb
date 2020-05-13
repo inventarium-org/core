54cfe5fabef272cd997d6c18e7d40a9740aac9f3
Anton Davydov <antondavydov.o@gmail.com>
2020-05-14 00:18:05 +0300

    Create simple list method for organisation_audit_item repository

--- lib/core/repositories/organisation_audit_item_repository.rb
+++ lib/core/repositories/organisation_audit_item_repository.rb
@@ -5,4 +5,12 @@ class OrganisationAuditItemRepository < Hanami::Repository
     belongs_to :organisation
     belongs_to :service
   end
+
+  def list(organisation_id, key: nil, limit: nil)
+    relation = root.where(organisation_id: organisation_id)
+    relation = relation.where(service_key: key) if key
+    relation = relation.limit(limit) if limit
+
+    relation.order { created_at.desc}.map_to(OrganisationAuditItem).to_a
+  end
 end
--- spec/core/repositories/organisation_audit_item_repository_spec.rb
+++ spec/core/repositories/organisation_audit_item_repository_spec.rb
@@ -1,5 +1,81 @@
 # frozen_string_literal: true
 
 RSpec.describe OrganisationAuditItemRepository, type: :repository do
-  # place your tests here
+  let(:repo) { described_class.new }
+
+  describe '#list' do
+    subject { repo.list(organisation.id, key: key, limit: limit) }
+
+    let(:organisation) { Fabricate(:organisation) }
+
+    before do
+      repo.create(organisation_id: organisation.id, service_key: 'test-audit', payload: {})
+      repo.create(organisation_id: organisation.id, service_key: 'test-audit', payload: {})
+      repo.create(organisation_id: organisation.id, service_key: 'test-audit', payload: {})
+      repo.create(organisation_id: organisation.id, service_key: 'other-audit', payload: {})
+    end
+
+    context 'when key and limit are empty' do
+      let(:key) { nil }
+      let(:limit) { nil }
+
+      it do
+        result = subject
+        expect(result).to be_a(Array)
+        expect(result.count).to eq(4)
+        expect(result).to all(be_a(OrganisationAuditItem))
+        expect(result.map(&:organisation_id)).to all(eq(organisation.id))
+        expect(result.map(&:service_key).uniq).to eq(['other-audit', 'test-audit'])
+      end
+    end
+
+    context 'when key is not empty' do
+      let(:key) { 'test-audit' }
+      let(:limit) { nil }
+
+      it do
+        result = subject
+        expect(result).to be_a(Array)
+        expect(result.count).to eq(3)
+        expect(result).to all(be_a(OrganisationAuditItem))
+        expect(result.map(&:organisation_id)).to all(eq(organisation.id))
+        expect(result.map(&:service_key).uniq).to eq(['test-audit'])
+      end
+    end
+
+    context 'when limit is not empty' do
+      let(:key) { nil }
+      let(:limit) { 2 }
+
+      it do
+        result = subject
+        expect(result).to be_a(Array)
+        expect(result.count).to eq(2)
+        expect(result).to all(be_a(OrganisationAuditItem))
+        expect(result.map(&:organisation_id)).to all(eq(organisation.id))
+        expect(result.map(&:service_key).uniq).to eq(['other-audit', 'test-audit'])
+      end
+    end
+
+    context 'when key and limit are not empty' do
+      let(:key) { 'test-audit' }
+      let(:limit) { 2 }
+
+      it do
+        result = subject
+        expect(result).to be_a(Array)
+        expect(result.count).to eq(2)
+        expect(result).to all(be_a(OrganisationAuditItem))
+        expect(result.map(&:organisation_id)).to all(eq(organisation.id))
+        expect(result.map(&:service_key).uniq).to eq(['test-audit'])
+      end
+    end
+
+    context 'when key is not exist' do
+      let(:key) { 'not-exist' }
+      let(:limit) { 2 }
+
+      it { expect(subject).to eq([]) }
+    end
+  end
 end
